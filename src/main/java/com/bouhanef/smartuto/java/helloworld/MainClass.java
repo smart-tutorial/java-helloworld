57b2bf116ca0488d4a3a4190cd63d1b4f311a04b
Cyber man <cyber.men.84@gmail.com>
2016-09-27 10:39:06 +0200

    Initial commit

---
+++ src/main/java/com/bouhanef/smartuto/java/helloworld/MainClass.java
@@ -0,0 +1,12 @@
+package com.bouhanef.smartuto.java.helloworld;
+
+/**
+ * Created by <a href="mailto:cyber.men.84@gmail.com">cyberMan</a> on 27/09/2016.
+ */
+public class MainClass {
+
+    public static void main(String[] ars) {
+        System.out.println("Hello world");
+    }
+
+}
